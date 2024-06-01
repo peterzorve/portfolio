f4cc2b2cc2694b7e615bec19e0a3a12eed7f86c3
Peter Zorve <98400766+peterzorve@users.noreply.github.com>
2024-06-02 00:19:33 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  // base: '/portfolio/',
+  base: '/',
   plugins: [preact()],
 })
